585ad4d5e9e19b3d3c7c0845006b5b1efdcb445e
FlaviaUERJ <79607025+FlaviaUERJ@users.noreply.github.com>
2021-05-23 23:37:51 -0300

    Update Exercicio.java

--- 03 - Interfaces/4.2/Exercicio.java
+++ 03 - Interfaces/4.2/Exercicio.java
@@ -7,7 +7,8 @@ Classe abstrata Pessoa
 
 B) Justificativa
 
-A classe abstrata é utilizada quando você quer que uma classe que herde dela implemente alguns métodos, mas que também possam ter métodos implementados na própria classe abstrata.
+A classe abstrata é utilizada quando você quer que uma classe que herde dela implemente alguns métodos, 
+mas que também possam ter métodos implementados na própria classe abstrata.
 
 C) Esboço do Código
 Está abaixo
@@ -39,7 +40,9 @@ A)Caso representativo
     Interface Controle Remoto.
 
 B) Justificativa
-    em interfaces, vc só coloca a assinatura dos métodos mas sem implementá-los ou com um corpo definido, pois todos os métodos de uma interface são public abstract, logo quem implementar uma interface terá de implementar todos os métodos que estão escritos nela.
+    em interfaces, vc só coloca a assinatura dos métodos mas sem implementá-los ou com um corpo definido, 
+    pois todos os métodos de uma interface são public abstract, logo quem implementar uma interface terá 
+    de implementar todos os métodos que estão escritos nela.
 
 C) esboço do código
 Está abaixo
@@ -56,22 +59,4 @@ Está abaixo
         boolean desligar();
 }
 
-
-
-
-
-
-
-
-
-
-
-
-b) Justificativa
-A classe abstrata é utilizada quando você quer que uma classe que herde dela implemente alguns métodos, mas que também possam ter métodos implementados na própria classe abstrata.
-
-c) Esboço do Código
-Está abaixo
-
-*/
 
